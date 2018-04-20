684e3dfb178fc4daf451554981f474ff9c881c38
Marci <moczarmarcell@gmail.com>
2018-04-20 10:47:03 +0200

    Javadoc kommentek hozzaadva.

--- skeleton/skeleton/TrapDoor.java
+++ skeleton/skeleton/TrapDoor.java
@@ -1,41 +1,65 @@
 import java.io.IOException;
 
 
-// A kapcsolóval aktivált lyukakat reprezentálja
+/**
+ * A csapoajtot reprezentalja. Allapotatol fugg, hogy egyszeru mezokent (Tile), vagy lyukkent (Hole) viselkedik.
+ */
 public class TrapDoor extends Tile {
-	
-	// alapbol zarva
+
+	/**
+	 * Alapbol zarva
+	 */
 	private boolean state = false;
 	//protected Switch Switch;
 
-	// Default konstruktor
+	/**
+	 * Default konstruktor
+	 * @throws IOException
+	 */
 	public TrapDoor() throws IOException {
 
 	}
 
-	// Csapóajtó állapotának lekérdése
+	/**
+	 * Csapoajto allapotanak lekerdezese
+	 * @return A csapoajto allapota.
+	 * @throws IOException
+	 */
 	public boolean GetState() throws IOException {
 
 		return state;
 	}
 
-	// Csapóajtó állapotának átváltása
+	/**
+	 * Csapoajto allapotanak atvaltasa
+	 */
 	public void SetState() {
 		state = !state;
 	}
-	
-	// Debug fn
+
+	/**
+	 * Debug fuggveny
+	 */
 	public void Hi() {
 		System.out.print("T");
 	}
-	
-	// eloszor ralep az entitas, es ha nyitva van, akkor leejti
+
+	/**
+	 * Eloszor ralep az entitas, es ha nyitva van, akkor leejti
+	 * @param e
+	 * @param d
+	 * @throws IOException
+	 */
 	public void Accept(Entity e, Direction d) throws IOException {
 		super.Accept(e, d);
 		if(state) DropEntity(e); // ha nyitva, leejti
 	}
 
-	// A mezőn lévő enity leejtése
+	/**
+	 * A mezon levo enity leejtese
+	 * @param e
+	 * @throws IOException
+	 */
 	public void DropEntity(Entity e) throws IOException {
 
 	}
