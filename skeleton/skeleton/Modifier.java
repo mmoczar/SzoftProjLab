20640156985b07a5559303071b89ce5d2d77822a
tbenedek <t.benedek10@gmail.com>
2018-04-22 22:20:55 +0200

    Resistance enumba pow int-be v2

--- skeleton/skeleton/Modifier.java
+++ skeleton/skeleton/Modifier.java
@@ -1,30 +1,17 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 /**
  * Az egyes mezok sikossagat hatarozza meg.
  */
 public enum Modifier {
-	/**
-	 * Tiszta
-	 */
-	CLEAN,
-	/**
-	 * Olajos
-	 */
-	OILY,
-	/**
-	 * Mezes
-	 */
-	HONEYED
-=======
-=======
->>>>>>> 127b4c3f83d36b16b32f1d6b145e3baf199da9a0
-public enum Modifier {
+    /**
+     * Tiszta
+     */
     REGULAR,
+    /**
+     * Olajos
+     */
     OIL,
+    /**
+     * Mezes
+     */
     HONEY
-<<<<<<< HEAD
->>>>>>> 127b4c3f83d36b16b32f1d6b145e3baf199da9a0
-=======
->>>>>>> 127b4c3f83d36b16b32f1d6b145e3baf199da9a0
 }
